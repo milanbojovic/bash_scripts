9e2ab14c285fbe23c674cde946f60c92ed18eb75
Zeus USER <bm040523d@student.etf.rs>
2016-01-19 02:32:17 +0000

    Script completed - sending notification emails after successfull completion

--- endava/bum_catering_crawler/bum_credit_checker.sh
+++ endava/bum_catering_crawler/bum_credit_checker.sh
@@ -1,13 +1,12 @@
-#******	Author ******
-#******	Milan Bojovic ******
-#****** Endava ******
 #!/bin/bash
 
 #Bum catering google sheed cravler 
 # This script checks if my money is decuced from my account on daily basis in regular manner (only 250 should be deduced)
 
+script_dir=/home/milanbojovic/bash_scripts/endava/bum_catering_crawler/
+
 #Read local cash balance history
-old_balance=`cat ./bum_credit`
+old_balance=`cat "$script_dir"bum_credit`
 echo "Old balance = $old_balance"
 
 #Read current cash balance from web
@@ -15,23 +14,37 @@ new_balance=`curl "https://docs.google.com/spreadsheets/d/1nxl17cqeyCsTYNYJ2GmTP
 echo "New balance = $new_balance"
 
 #Create difference variable
-let difference=old_balance-new_balance
+difference=$((old_balance - new_balance))
 echo "Difference = $difference"
 
+output_line_base="`date`, old_balance=$old_balance din, new_balance=$new_balance din, diff= $difference din.  --> "
 
 #Analyze data and create log file
-if (( $difference <= 250 )); then
-	if (( $difference >= 0 )); then
-		echo "`date`, old_balance=$old_balance din, new_balance=$new_balance din, diff=$difference din.  -->  OK" | tee -a ./bum_credit_history
+if [ $difference -lt 250 ]
+then
+	if [ $difference -ge 0 ]
+	then
+		output_line_full="$output_line_base OK"
+		echo "$output_line_full" | tee -a "$script_dir"bum_credit_history
 	else
-		echo "`date`, old_balance=$old_balance din, new_balance=$new_balance din, diff=$difference din.  -->  OK - !!!CREDIT ADDED!!!" | tee -a ./bum_credit_history
+		output_line_full="$output_line_base OK - !!!CREDIT ADDED!!!"
+		echo "$output_line_full" | tee -a "$script_dir"bum_credit_history
 	fi
 else 
-	echo "`date`, old_balance=$old_balance din, new_balance=$new_balance din, diff=$difference din.  -->  ERROR" | tee -a ./bum_credit_history
+	output_line_full="$output_line_base ERROR"
+	echo "$output_line_full" | tee -a "$script_dir"bum_credit_history
 fi
 
 #Write new balance to local balance file
-echo $new_balance > bum_credit
-
+echo $new_balance > "$script_dir"bum_credit
 
 #Send email !
+/usr/bin/mail -s "BUM INFO" milan.bojovic@endava.com << EOF
+
+===========================================================================================
+					DAILY BALANCE:
+===========================================================================================
+$output_line_full
+===========================================================================================
+
+EOF
